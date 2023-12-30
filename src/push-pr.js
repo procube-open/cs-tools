40576da0f7a4800348b89aaaed8affccda1b123e
Mitsuru Nakakawaji <mitsuru@procube.jp>
2023-12-30 10:22:30 +0900

    support argv of command line (#4)
    * support argv of command line
    
    * modify README.md

--- src/push-pr.js
+++ src/push-pr.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
 const { spawn } = require('node:child_process');
-const p = spawn(__dirname + '/../cs/push-pr.sh', {stdio: 'inherit'});
+const p = spawn(__dirname + '/../cs/push-pr.sh', process.argv.slice(2), {stdio: 'inherit'});
 p.on('close', (code) => process.exit(code))
